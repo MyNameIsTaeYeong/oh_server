2ef52f69a3e4e6216a2cb626212c9c468ed4988f
TaeYeong Im <imtaebari@gmail.com>
2022-05-26 19:38:18 +0900

    테스트 이름 구체화

--- __tests__/domains/Activity.test.js
+++ __tests__/domains/Activity.test.js
@@ -1,18 +1,18 @@
 const Activity = require("../../domains/Activity");
 
-test("set id & get id", () => {
+test("Activity set id & get id", () => {
   const activity = new Activity();
   activity.id = 1;
   expect(activity.id).toBe(1);
 });
 
-test("set name & get name", () => {
+test("Activity set name & get name", () => {
   const activity = new Activity();
   activity.name = "jaja";
   expect(activity.name).toBe("jaja");
 });
 
-test("set userId & get userId", () => {
+test("Activity set userId & get userId", () => {
   const activity = new Activity();
   activity.userId = 1;
   expect(activity.userId).toBe(1);
--- __tests__/domains/Emotion.test.js
+++ __tests__/domains/Emotion.test.js
@@ -1,18 +1,18 @@
 const Emotion = require("../../domains/Emotion");
 
-test("set id & get id", () => {
+test("Emotion set id & get id", () => {
   const emotion = new Emotion();
   emotion.id = 1;
   expect(emotion.id).toBe(1);
 });
 
-test("set name & get name", () => {
+test("Emotion set name & get name", () => {
   const emotion = new Emotion();
   emotion.name = "jaja";
   expect(emotion.name).toBe("jaja");
 });
 
-test("set userId & get userId", () => {
+test("Emotion set userId & get userId", () => {
   const emotion = new Emotion();
   emotion.userId = 1;
   expect(emotion.userId).toBe(1);
