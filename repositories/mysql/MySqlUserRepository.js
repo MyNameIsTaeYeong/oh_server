38160e05c64c861df46ad253d8ee6f61c2cceae5
TaeYeong Im <imtaebari@gmail.com>
2022-06-15 17:04:34 +0900

    MySqlUserRepository 예외처리, testcode 통합 -> 단위로 변경

--- repositories/mysql/MySqlUserRepository.js
+++ repositories/mysql/MySqlUserRepository.js
@@ -1,3 +1,6 @@
+const ReadError = require("../../errors/ReadError");
+const UnexpectedError = require("../../errors/UnexpectedError");
+const WriteError = require("../../errors/WriteError");
 const UserRepository = require("../UserRepository");
 
 class MySqlUserRepository extends UserRepository {
@@ -9,33 +12,98 @@ class MySqlUserRepository extends UserRepository {
   }
 
   async save(user) {
-    return (
-      await this.#POOL.execute(`INSERT INTO Users(email) VALUES(?)`, [
-        user.email,
-      ])
-    )[0].insertId;
+    try {
+      return (
+        await this.#POOL.execute(`INSERT INTO Users(email) VALUES(?)`, [
+          user.email,
+        ])
+      )[0].insertId;
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async remove(user) {
-    await this.#POOL.execute(`DELETE FROM Users WHERE email=?`, [user.email]);
+    try {
+      await this.#POOL.execute(`DELETE FROM Users WHERE email=?`, [user.email]);
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findByEmail(user) {
-    return (
-      await this.#POOL.execute(`SELECT * FROM Users WHERE email=?`, [
-        user.email,
-      ])
-    )[0][0];
+    try {
+      return (
+        await this.#POOL.execute(`SELECT * FROM Users WHERE email=?`, [
+          user.email,
+        ])
+      )[0][0];
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new ReadError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new ReadError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new ReadError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findById(user) {
-    return (
-      await this.#POOL.execute(`SELECT * FROM Users WHERE id=?`, [user.id])
-    )[0][0];
+    try {
+      return (
+        await this.#POOL.execute(`SELECT * FROM Users WHERE id=?`, [user.id])
+      )[0][0];
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new ReadError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new ReadError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new ReadError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findAll() {
-    return (await this.#POOL.execute(`SELECT * FROM Users`))[0];
+    try {
+      return (await this.#POOL.execute(`SELECT * FROM Users`))[0];
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new ReadError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new ReadError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new ReadError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async clear() {
