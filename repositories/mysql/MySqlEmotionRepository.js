49c0d381811c96e045ee71d79cb8571c1bee00d6
TaeYeong Im <imtaebari@gmail.com>
2022-06-15 16:41:19 +0900

    MySqlEmotionRepository 예외처리, testcode 통합 -> 단위로 변경

--- repositories/mysql/MySqlEmotionRepository.js
+++ repositories/mysql/MySqlEmotionRepository.js
@@ -1,3 +1,6 @@
+const ReadError = require("../../errors/ReadError");
+const UnexpectedError = require("../../errors/UnexpectedError");
+const WriteError = require("../../errors/WriteError");
 const RecordRepository = require("../RecordRepository");
 
 class MySqlEmotionRepository extends RecordRepository {
@@ -9,24 +12,63 @@ class MySqlEmotionRepository extends RecordRepository {
   }
 
   async save(emotion) {
-    return (
-      await this.#POOL.execute(
-        `INSERT INTO Emotions(name, userId) VALUES(?, ?)`,
-        [emotion.name, emotion.userId]
-      )
-    )[0].insertId;
+    try {
+      return (
+        await this.#POOL.execute(
+          `INSERT INTO Emotions(name, userId) VALUES(?, ?)`,
+          [emotion.name, emotion.userId]
+        )
+      )[0].insertId;
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async remove(emotion) {
-    await this.#POOL.execute(`DELETE FROM Emotions WHERE id=?`, [emotion.id]);
+    try {
+      await this.#POOL.execute(`DELETE FROM Emotions WHERE id=?`, [emotion.id]);
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findAll(user) {
-    return (
-      await this.#POOL.execute(`SELECT * FROM Emotions WHERE userId=?`, [
-        user.id,
-      ])
-    )[0];
+    try {
+      return (
+        await this.#POOL.execute(`SELECT * FROM Emotions WHERE userId=?`, [
+          user.id,
+        ])
+      )[0];
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new ReadError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new ReadError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new ReadError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findById(emotion) {
