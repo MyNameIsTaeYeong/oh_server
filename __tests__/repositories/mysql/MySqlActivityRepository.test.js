f95c4329ca0d328ac02431f59bbf4ba9f3dac4ed
TaeYeong Im <imtaebari@gmail.com>
2022-06-15 16:18:10 +0900

    MySqlActivityRepository 예외처리, testcode 통합 -> 단위로 변경

--- __tests__/repositories/mysql/MySqlActivityRepository.test.js
+++ __tests__/repositories/mysql/MySqlActivityRepository.test.js
@@ -1,50 +1,196 @@
 const Activity = require("../../../domains/Activity");
 const User = require("../../../domains/User");
-require("../../../container");
+const ReadError = require("../../../errors/ReadError");
+const UnexpectedError = require("../../../errors/UnexpectedError");
+const WriteError = require("../../../errors/WriteError");
+const MySqlActivityRepository = require("../../../repositories/mysql/MySqlActivityRepository");
+
 const Container = require("typedi").Container;
 
-let user;
-const mySqlUserRepository = Container.get("UserRepository");
-const mySqlActivityRepository = Container.get("ActivityRepository");
-
-beforeAll(async () => {
-  user = new User("test");
-  user.id = await mySqlUserRepository.save(user);
+afterEach(() => {
+  Container.remove("POOL");
 });
 
-afterAll(async () => {
-  await mySqlUserRepository.clear();
+describe("MySqlActivityRepository의 save()", () => {
+  test("save()는 저장된 활동 아이디를 반환해야 한다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.resolve([{ insertId: 1 }]);
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(await mySqlActivityRepository.save(new Activity())).toBe(1);
+  });
+
+  test("save()는 ER_ACCESS_DENIED_ERROR 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ER_ACCESS_DENIED_ERROR" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.save(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("save()는 ECONNREFUSED 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ECONNREFUSED" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.save(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("save()는 PROTOCOL_CONNECTION_LOST 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "PROTOCOL_CONNECTION_LOST" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.save(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("save()는 그 외의 에러시 UnexpectedError 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "그 외" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.save(new Activity())
+    ).rejects.toThrowError(UnexpectedError);
+  });
 });
 
-afterEach(async () => {
-  await mySqlActivityRepository.clear();
+describe("MySqlActivityRepository의 remove()", () => {
+  test("remove()는 ER_ACCESS_DENIED_ERROR 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ER_ACCESS_DENIED_ERROR" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.remove(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("remove()는 ECONNREFUSED 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ECONNREFUSED" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.remove(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("remove()는 PROTOCOL_CONNECTION_LOST 에러시 WriteError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "PROTOCOL_CONNECTION_LOST" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.remove(new Activity())
+    ).rejects.toThrowError(WriteError);
+  });
+
+  test("remove()는 그 외의 에러시 UnexpectedError 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "그 외" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(async () =>
+      mySqlActivityRepository.remove(new Activity())
+    ).rejects.toThrowError(UnexpectedError);
+  });
 });
 
-test("MySqlActivityRepository의 save()는 활동 아이디를 반환한다.", async () => {
-  const activity = new Activity("운동", user.id);
-  activity.id = await mySqlActivityRepository.save(activity);
-  const expectedId = (await mySqlActivityRepository.findById(activity)).id;
-  expect(expectedId).toBe(activity.id);
-});
+describe("MySqlActivityRepository의 findAll()", () => {
+  test("findAll()는 저장된 활동 아이디를 반환해야 한다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.resolve([[{}, {}]]);
+      },
+    });
 
-test("MySqlActivityRepository의 findAll()은 유저의 모든 활동을 반환한다.", async () => {
-  const activity = new Activity("운동", user.id);
-  activity.id = await mySqlActivityRepository.save(activity);
-  const activity2 = new Activity("공부", user.id);
-  activity2.id = await mySqlActivityRepository.save(activity2);
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect((await mySqlActivityRepository.findAll(new User())).length).toBe(2);
+  });
 
-  const activities = await mySqlActivityRepository.findAll(user);
+  test("save()는 ER_ACCESS_DENIED_ERROR 에러시 ReadError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ER_ACCESS_DENIED_ERROR" });
+      },
+    });
 
-  expect(activities).toStrictEqual([
-    { id: activity.id, name: activity.name, userId: activity.userId },
-    { id: activity2.id, name: activity2.name, userId: activity2.userId },
-  ]);
-});
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(
+      async () => await mySqlActivityRepository.findAll(new User())
+    ).rejects.toThrowError(ReadError);
+  });
 
-test("MySqlActivityRepository의 remove()은 해당 활동을 삭제한다.", async () => {
-  const activity = new Activity("운동", user.id);
-  activity.id = await mySqlActivityRepository.save(activity);
-  await mySqlActivityRepository.remove(activity);
-  const expected = await mySqlActivityRepository.findById(activity);
-  expect(expected).toStrictEqual(undefined);
+  test("save()는 ECONNREFUSED 에러시 ReadError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "ECONNREFUSED" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(
+      async () => await mySqlActivityRepository.findAll(new User())
+    ).rejects.toThrowError(ReadError);
+  });
+
+  test("save()는 PROTOCOL_CONNECTION_LOST 에러시 ReadError를 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "PROTOCOL_CONNECTION_LOST" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(
+      async () => await mySqlActivityRepository.findAll(new User())
+    ).rejects.toThrowError(ReadError);
+  });
+
+  test("save()는 그 외의 에러시 UnexpectedError 던진다.", async () => {
+    Container.set("POOL", {
+      execute: (param1, param2) => {
+        return Promise.reject({ code: "그 외" });
+      },
+    });
+
+    const mySqlActivityRepository = new MySqlActivityRepository(Container);
+    expect(
+      async () => await mySqlActivityRepository.findAll(new User())
+    ).rejects.toThrowError(UnexpectedError);
+  });
 });
--- repositories/mysql/MySqlActivityRepository.js
+++ repositories/mysql/MySqlActivityRepository.js
@@ -1,3 +1,6 @@
+const ReadError = require("../../errors/ReadError");
+const UnexpectedError = require("../../errors/UnexpectedError");
+const WriteError = require("../../errors/WriteError");
 const RecordRepository = require("../RecordRepository");
 
 class MySqlActivityRepository extends RecordRepository {
@@ -9,26 +12,65 @@ class MySqlActivityRepository extends RecordRepository {
   }
 
   async save(activity) {
-    return (
-      await this.#POOL.execute(
-        `INSERT INTO Activities(name, userId) VALUES(?, ?)`,
-        [activity.name, activity.userId]
-      )
-    )[0].insertId;
+    try {
+      return (
+        await this.#POOL.execute(
+          `INSERT INTO Activities(name, userId) VALUES(?, ?)`,
+          [activity.name, activity.userId]
+        )
+      )[0].insertId;
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async remove(activity) {
-    await this.#POOL.execute(`DELETE FROM Activities WHERE id=?`, [
-      activity.id,
-    ]);
+    try {
+      await this.#POOL.execute(`DELETE FROM Activities WHERE id=?`, [
+        activity.id,
+      ]);
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new WriteError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new WriteError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new WriteError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findAll(user) {
-    return (
-      await this.#POOL.execute(`SELECT * FROM Activities WHERE userId=?`, [
-        user.id,
-      ])
-    )[0];
+    try {
+      return (
+        await this.#POOL.execute(`SELECT * FROM Activities WHERE userId=?`, [
+          user.id,
+        ])
+      )[0];
+    } catch (error) {
+      switch (error.code) {
+        case "ER_ACCESS_DENIED_ERROR":
+          throw new ReadError("MySql Server Error", error);
+        case "ECONNREFUSED":
+          throw new ReadError("Nodejs Error", error);
+        case "PROTOCOL_CONNECTION_LOST":
+          throw new ReadError("Connection Lost", error);
+        default:
+          throw new UnexpectedError("UnexpectedError", error);
+      }
+    }
   }
 
   async findById(activity) {
