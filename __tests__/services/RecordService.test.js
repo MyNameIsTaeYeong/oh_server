553c14b6c666aabb79baee9004e2c601dd8e4949
TaeYeong Im <imtaebari@gmail.com>
2022-06-28 13:25:45 +0900

    RecordService test 보충

--- __tests__/services/RecordService.test.js
+++ __tests__/services/RecordService.test.js
@@ -9,6 +9,22 @@ afterEach(() => {
 });
 
 describe("RecordService의 createRecord", () => {
+  test("createRecord는 생성된 기록의 아이디를 반환한다.", async () => {
+    // given
+    const insertId = 1;
+    Container.set("RecordRepository", {
+      save: () => Promise.resolve(insertId),
+    });
+    const recordService = new RecordService(Container);
+
+    // when
+    const result = await recordService.createRecord(
+      new Activity({ name: "test", userId: 1 })
+    );
+
+    // then
+    expect(result).toBe(insertId);
+  });
   test("record의 name이 없으면 ArgumentError를 던진다.", async () => {
     Container.set("RecordRepository", {});
     const recordService = new RecordService(Container);
@@ -55,6 +71,20 @@ describe("RecordService의 deleteRecord", () => {
 });
 
 describe("RecordService의 selectRecords", () => {
+  test("selectRecords는 유저의 모든 기록을 반환한다.", async () => {
+    // given
+    Container.set("RecordRepository", {
+      findAll: () => Promise.resolve([{ name: "기록1" }, { name: "기록2" }]),
+    });
+    const recordService = new RecordService(Container);
+
+    // when
+    const result = await recordService.selectRecords(new User({ id: 1 }));
+
+    // then
+    expect(result).toStrictEqual([{ name: "기록1" }, { name: "기록2" }]);
+  });
+
   test("user의 id가 없으면 ArgumentError를 던진다.", async () => {
     Container.set("RecordRepository", {});
     const recordService = new RecordService(Container);
